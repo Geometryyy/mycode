e657a1f432e8134d0edd72df040b07bbe88adabe
Geometryyy <3470498828@qq.com>
2024-09-26 23:22:02 +0800

    111

--- model.py
+++ model.py
@@ -106,34 +106,22 @@ class MyLlava(nn.Module, GenerationMixin):
             )
         # generation with cache
         elif past_key_values is not None and pixel_values is None and input_ids.shape[1] == 1:
-            # Retrieve the first layer to inspect the logits and mask out the hidden states
-            # that are set to 0
             first_layer_past_key_value = past_key_values[0][0][:, :, :, 0]
-
-            # Sum all dimensions of head_dim (-2) to avoid random errors such as: https://github.com/huggingface/transformers/pull/28032#issuecomment-1863691941
-            batch_index, non_attended_tokens = torch.where(first_layer_past_key_value.float().sum(-2) == 0)
-
-            # Get the target length
-            target_length = input_ids.shape[1]
+            batch_index, non_attended_token_ids = torch.where(first_layer_past_key_value.float().sum(-2) == 0)
             past_length = first_layer_past_key_value.shape[-1]
-
-            extended_attention_mask = torch.ones(
-                (attention_mask.shape[0], past_length),
-                dtype=attention_mask.dtype,
-                device=attention_mask.device,
-            )
+            extended_attention_mask = torch.ones((attention_mask.shape[0], past_length), dtype=attention_mask.dtype, device=attention_mask.device)
 
             # Filter out only the tokens that can be un-attended, this can happen
             # if one uses Llava + Fused modules where the cache on the
             # first iteration is already big enough, or if one passes custom cache
-            valid_indices = non_attended_tokens < extended_attention_mask.size(-1)
+            valid_indices = non_attended_token_ids < extended_attention_mask.size(-1)
             new_batch_index = batch_index[valid_indices]
-            new_non_attended_tokens = non_attended_tokens[valid_indices]
+            new_non_attended_tokens = non_attended_token_ids[valid_indices]
 
             # Zero-out the places where we don't need to attend
             extended_attention_mask[new_batch_index, new_non_attended_tokens] = 0
 
-            attention_mask = torch.cat((extended_attention_mask, attention_mask[:, -target_length:]), dim=1)
+            attention_mask = torch.cat((extended_attention_mask, attention_mask[:, -1:]), dim=1)
             position_ids = torch.sum(attention_mask, dim=1).unsqueeze(-1) - 1
 
         outputs = self.llm(
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 # os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"
 import json
@@ -10,54 +10,14 @@ from torch.utils.data import DataLoader
 from dataset import CC595kDataset, CC595kDataCollator
 from model import MyLlava, MyLlavaProjector
 
-class TrainingConfigurations:
-    # paths
-    checkpoint_path = '/mnt/zhaojingtong/checkpoints'
-    data_path = '/mnt/zhaojingtong/data/cc-595k'
-    output_dir = '/mnt/zhaojingtong/code/results'
-    # train
-    start_epoch = 0
-    train_epochs = 1
-    per_device_train_batch_size = 4
-    per_device_eval_batch_size = 4
-    learning_rate = 2e-3
-    weight_decay = 0
-    eval_strategy = 'steps'
-    save_strategy = 'steps'
-    # llm-conversation
-    system = "You are a helpful language and vision assistant. You are able to understand the visual content that the user provides, and assist the user with a variety of tasks using natural language."
-    sep = "</s>"
-    # model
-    ignore_index = -100
-    image_token_index = 128004
-    pad_token_id = 128002
-    projector_hidden_act = "gelu"
-    feature_layer = -2
-    image_encoder_hidden_size = 1024
-    llm_hidden_size = 4096
-    use_cache = False
+from transformers import AutoTokenizer, LlamaForCausalLM
 
+model = LlamaForCausalLM.from_pretrained("/mnt/zhaojingtong/checkpoints/Meta-Llama-3.1-8B").cuda()
+tokenizer = AutoTokenizer.from_pretrained("/mnt/zhaojingtong/checkpoints/tokenizer")
 
-# config
-cfg = TrainingConfigurations()
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
 
-# make checkpoint directories
-local_time = localtime(time())
-time_info = '' if (cfg.start_epoch != 0) else "{}-{}-{}-{}".format(local_time.tm_mon, local_time.tm_mday, local_time.tm_hour, local_time.tm_min)
-model_path = cfg.output_dir + f"/{time_info}"
-os.makedirs(model_path + "/models", exist_ok=True)
-os.makedirs(model_path + "/log", exist_ok=True)
-json_string = json.dumps(cfg.__dict__, indent=4)
-with open(model_path + "/config.json", 'w') as file:
-    file.write(json_string)
-
-# model
-tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3.1-8B')
-tokenizer.pad_token = '<pad>'
-tokenizer.pad_token_id = 128002
-tokenizer.unk_token = '<unk>'
-tokenizer.unk_token_id = 128003
-tokenizer.sep_token = '<image>'
-tokenizer.sep_token_id = 128004
-# 确保新符号的ID与原符号的ID相同
-tokenizer.save_pretrained(os.path.join(cfg.checkpoint_path, 'tokenizer'))
+# Generate
+generate_ids = model(inputs.input_ids, use_cache=True)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
